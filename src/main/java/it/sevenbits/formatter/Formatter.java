cf5f831abca374a0ba3eb0e51aa719bfa8a1f720
Alexey Butko <abutko94@gmail.com>
2016-10-28 10:12:59 +0600

    Build pom.xml + checkstyle

--- src/main/java/it/sevenbits/formatter/Formatter.java
+++ src/main/java/it/sevenbits/formatter/Formatter.java
@@ -1,48 +1,56 @@
+/**
+ * Package containing Formatter method
+ */
 package it.sevenbits.formatter;
 
 
 class Formatter {
-     static void checkSymbol(String s){
+     static void checkSymbol(String s) {
 
-        int k = 0;
+        int block = 0;
+        boolean tag = false;
         for (int i = 0; i < s.length(); i++) {
 
-            char c = s.charAt(i);
+            char symbol = s.charAt(i);
 
-            if ((c != '{') && (c != ';') && (c != '}')) {
-                System.out.print(c);
-            } else {
-                switch (c) {
-                    case '{': {
-                        System.out.println(c);
-                        k = k + 1;
-                        for (int j = 0; j < k; j++) {
-                            System.out.print(" ");
-
-                        }
+                 switch (symbol) {
+                    case '{':
+                        block++;
+                        System.out.println(symbol);
+                        tab(block);
                         break;
-                    }
-                    case ';': {
-                        System.out.println(c);
-                        for (int j = 0; j < k; j++) {
-                            System.out.print(" ");
-                        }
-                        break;
-                    }
-                    case '}': {
-                        k = k - 1;
 
-                        System.out.println(c);
-                        for (int j = 0; j < k; j++) {
-                            System.out.print(" ");
-
-                        }
+                    case ';':
+                        tag = true;
+                        System.out.println(symbol);
                         break;
-                    }
+
+                    case '}':
+                        block--;
+                        tab(block);
+                        System.out.println(symbol);
+                        break;
+
                     default:
+                        if (tag) {
+                            tab(block);
+                            System.out.print(symbol);
+                            tag = false;
+                        } else {
+                            System.out.print(symbol);
+                        }
                         break;
                 }
-            }
+        }
+     }
+    /**
+     * method tab
+
+     */
+    static void tab(final int block) {
+        for (int i = 0; i < block; i++) {
+            System.out.print("\t");
         }
     }
 }
+
--- src/main/java/it/sevenbits/formatter/Main.java
+++ src/main/java/it/sevenbits/formatter/Main.java
@@ -6,8 +6,7 @@ package it.sevenbits.formatter;
 public class Main {
 
     public static void main(final String[] args) {
-        String s = "int i=34;int c=7;if(i>2){int k=++;}";
+        String s = "int i=34;int c=7;if(i>2){int k=++;if((k=1)&&(k!=2)){switch (c){int 5;int r;}}}";
         Formatter.checkSymbol(s);
-
     }
 }
