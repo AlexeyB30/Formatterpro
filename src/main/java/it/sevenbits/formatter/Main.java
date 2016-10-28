8bb030ad2c5af2071f1d9342c3e69df9e9ba99f0
Alexey Butko <abutko94@gmail.com>
2016-10-28 09:17:02 +0600

    Build pom.xml + checkstyle

--- src/main/java/it/sevenbits/formatter/Main.java
+++ src/main/java/it/sevenbits/formatter/Main.java
@@ -7,45 +7,7 @@ public class Main {
 
     public static void main(final String[] args) {
         String s = "int i=34;int c=7;if(i>2){int k=++;}";
-        int k = 0;
-        for (int i = 0; i < s.length(); i++) {
+        Formatter.checkSymbol(s);
 
-            char c = s.charAt(i);
-
-            if ((c != '{') && (c != ';') && (c != '}')) {
-                System.out.print(c);
-            } else {
-                switch (c) {
-                    case '{': {
-                        System.out.println(c);
-                        k = k + 1;
-                        for (int j = 0; j < k; j++) {
-                            System.out.print(" ");
-
-                        }
-                        break;
-                    }
-                    case ';': {
-                        System.out.println(c);
-                        for (int j = 0; j < k; j++) {
-                            System.out.print(" ");
-                        }
-                        break;
-                    }
-                    case '}': {
-                        k = k - 1;
-
-                        System.out.println(c);
-                        for (int j = 0; j < k; j++) {
-                            System.out.print(" ");
-
-                        }
-                        break;
-                    }
-                    default:
-                        break;
-                }
-            }
-        }
     }
 }
